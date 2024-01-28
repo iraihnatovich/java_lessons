494653dfbd2a2f3e0e594c18986239e10ae1f94f
iihnatovich <i.ihnatovich@gmail.com>
2024-01-28 12:33:46 +0300

    lesson 10 task1,2,3

---
+++ src/by/teachmeskills/lesson10/task2/Task2.java
@@ -0,0 +1,23 @@
+package by.teachmeskills.lesson10.task2;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String initial = scan.nextLine();
+        String[] setWords = initial.split(" ");
+        String minWord = setWords[0];
+        String maxWord = setWords[0];
+        for (String word : setWords) {
+            if (word.length() <= minWord.length()) {
+                minWord = word;
+            } else if (word.length() >= maxWord.length()) {
+                maxWord = word;
+            }
+        }
+        System.out.println("Longest word : " + maxWord);
+        System.out.println("Shortest word : " + minWord);
+
+    }
+}
---
+++ src/by/teachmeskills/lesson10/task3/Task3Demo.java
@@ -0,0 +1,21 @@
+package by.teachmeskills.lesson10.task3;
+
+import java.util.Scanner;
+
+import static by.teachmeskills.lesson10.task3.SymbolCalculation.uniqueSymbols;
+
+public class Task3Demo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String initial = scan.nextLine();
+        String[] strSet = initial.split(" ");
+        String minUnique = strSet[0];
+        for (int i = 0; i < strSet.length - 1; i++) {
+            if (uniqueSymbols(minUnique) > uniqueSymbols(strSet[i + 1])) {
+                minUnique = strSet[i + 1];
+            }
+        }
+        System.out.println(minUnique);
+    }
+
+}
