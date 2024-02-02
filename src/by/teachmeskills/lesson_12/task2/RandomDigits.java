b999ca56a27c1190b1a11a5d681d5cd19b05c5e7
iihnatovich <i.ihnatovich@gmail.com>
2024-02-02 13:04:58 +0300

    lesson 12 task 1, 2

---
+++ src/by/teachmeskills/lesson_12/task2/RandomDigits.java
@@ -0,0 +1,53 @@
+package by.teachmeskills.lesson_12.task2;
+
+import java.util.*;
+
+public class RandomDigits {
+
+    private static int setSizeFromConsole (){
+        Scanner scan = new Scanner(System.in);
+        try {
+            if (!scan.hasNextInt()) {
+                throw new InputMismatchException("Only integers. Enter again: ");
+            }
+            else {
+                return scan.nextInt();
+            }
+        } catch (InputMismatchException e) {
+            System.out.println(e.getMessage());
+            return setSizeFromConsole();
+        }
+    }
+
+    protected static void fillInRandom(List myList) {
+        Random random = new Random();
+        try {
+            int myListSize = setSizeFromConsole();
+            if (myListSize <= 1) {
+                throw new IncorrectSizeException("No sense to calculate average if size is <= 1. Enter size again: ");
+            }
+            for (int i = 0; i < myListSize; i++) {
+                myList.add(random.nextInt(-100,100));
+            }
+        } catch (IncorrectSizeException e) {
+            System.out.println(e.getMessage());
+             fillInRandom(myList);
+        }
+    }
+
+//    private static int sumElements(List myList){
+//        int sumElements = 0;
+//        for(int i =0; i<myList.size(); i++){
+//            sumElements  += (Integer) myList.get(i);
+//        }
+//        return sumElements;
+//    }
+    protected static double average (List myList){
+        int sumElements = 0;
+        for(int i =0; i<myList.size(); i++){
+            sumElements  += (Integer) myList.get(i);
+        }
+        return (double) sumElements /(double) myList.size();
+    }
+
+}
---
+++ src/by/teachmeskills/lesson_12/task2/Task2.java
@@ -0,0 +1,26 @@
+package by.teachmeskills.lesson_12.task2;
+
+import java.util.ArrayList;
+import java.util.List;
+import static by.teachmeskills.lesson_12.task2.RandomDigits.average;
+import static by.teachmeskills.lesson_12.task2.RandomDigits.fillInRandom;
+
+//Задача 2
+//        Создать коллекцию целых чисел.
+//        Заполните коллекцию рандомными числами.
+//        Пусть размер коллекции задается с консоли.
+//        Предусмотреть возможно ошибок при получении размера коллекции с консоли.
+//        Предусмотреть проверку введенного размера коллекции на валидность.
+//        Посчитайте и выведите на экран среднее арифметическое все элементов коллекции.
+public class Task2 {
+    public static void main(String[] args) {
+        List<Integer> intNumbers = new ArrayList<Integer>();
+        fillInRandom(intNumbers);
+        System.out.println(intNumbers);
+//        System.out.println(sumElements(intNumbers));
+        System.out.println(average(intNumbers));
+
+    }
+
+
+}
